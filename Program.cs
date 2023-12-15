60bd6b654df15c65f2c4fca62c8dc45fdab69490
Yuvaraj S R <yuvaraj.ramesh@aravind.org>
2023-12-15 09:24:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using System.Diagnostics;
+
+namespace AzureSTT
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await RecognizeSpeech();
+            Console.WriteLine("End");
+        }
+
+        private static async Task RecognizeSpeech()
+        {
+            var watch = Stopwatch.StartNew();
+
+            var configuration = SpeechConfig.FromSubscription("37f7fbca88bc408ca9d7a53c3ba0ab4e", "centralindia");
+            using (var recog = new SpeechRecognizer(configuration))
+            {
+                await Recurring(watch, recog);
+            }
+        }
+
+        private static async Task Recurring(Stopwatch watch, SpeechRecognizer recog)
+        {
+            Console.WriteLine("Speak Something...");
+            var result = await recog.RecognizeOnceAsync();
+            if (result.Reason == ResultReason.RecognizedSpeech)
+            {
+                watch.Stop();
+                Console.WriteLine(result.Text);
+                if (!watch.IsRunning)
+                {
+                    watch.Restart(); // Reset time to 0 and start measuring
+                    await Recurring(watch, recog);
+                }
+            }
+        }
+
+    }
+}
